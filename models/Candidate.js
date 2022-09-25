d9c5e773823f36ac8190c395cd2012208321485a
Hugo_Furtado <hugofurtadocof@gmail.com>
2022-09-24 22:07:45 -0300

    :bricks: add candidate model

---
+++ models/Candidate.js
@@ -0,0 +1,11 @@
+
+// initial model
+const mongoose = require('mongoose');
+const Candidate = mongoose.model('Candidate', {
+    name: String,
+    party: String,
+    salary: Number,
+    approved: Boolean,
+});
+
+module.exports = Candidate;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //  initial settings
 const express = require('express');
 const mongoose = require('mongoose');
+const Candidate = require('./models/Candidate');
 const app = express();
 const port = 3000;
 
@@ -11,6 +12,42 @@ app.use(express.urlencoded(
         extended: true
     }
 ));
+
+app.use(express.json());
+
+
+//  initial route
+app.get('/', (req, res) => {
+    res.json({ message: 'Hi' });
+});
+
+
+//  data create
+app.post('/candidate', async (req, res) => {
+
+    const {name, party, salary, approved} = req.body; // destructuring assignment
+
+    if(!name){
+        res.status(422).json({error: 'Name is required!'}) //  validation
+    }
+
+    const candidate = {
+        name,
+        party,
+        salary,
+        approved
+    };
+
+    try {
+
+        await Candidate.create(Candidate);
+        res.status(201).json({message: 'Candidate was created!'});
+
+    } catch (error) {
+        res.status(500).json({error: error});
+    }
+});
+
 
 
 // db connection
